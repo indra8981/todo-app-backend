f856d6726a6d9baecea5fdc0a902ae5f69303c51
Indrajit Sinha <indrajit@almabase.com>
2020-11-08 23:49:41 +0530

    Added Backend files

---
+++ build/precache-manifest.59d09b2dba22e5621c2e4b3f0078400b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "354f7cc4dc64536979bf9c33495fdde7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92fc72eee5489fa80245",
+    "url": "/static/css/2.89d568a6.chunk.css"
+  },
+  {
+    "revision": "40309d44937d01b622e9",
+    "url": "/static/css/main.111a37ce.chunk.css"
+  },
+  {
+    "revision": "92fc72eee5489fa80245",
+    "url": "/static/js/2.13a2011f.chunk.js"
+  },
+  {
+    "revision": "40309d44937d01b622e9",
+    "url": "/static/js/main.29f1c368.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = "mongodb+srv://schools:GC00eB1WtiM34a6D@schools.l9qgr.mongodb.net/<dbname6>?retryWrites=true&w=majority";
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
+const todosRouter = require('./routes/todos');
+
+app.use('/todos', todosRouter);
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
